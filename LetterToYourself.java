b11d68135769b24c28ad6bd1b6f19552d5e79b30
aadusei <estsitsecurity@gmail.com>
2018-05-27 21:30:57 -0500

    LetterToYourself
    LetterToYourself

---
+++ LetterToYourself.java
@@ -0,0 +1,21 @@
+
+public class LetterToYourself {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 System.out.println("+---------------------------------------------------------+");
+	        System.out.println("|                                                    #### |");
+	        System.out.println("|                                                    #### |");
+	        System.out.println("|                                                    #### |");
+	        System.out.println("|                                                         |");
+	        System.out.println("|                                                         |");
+	        System.out.println("|                              Alfred Adusei              |");
+	        System.out.println("|                              6601 Harbor Town Drive     |");
+	        System.out.println("|                              Houston,Texas 77036        |");
+	        System.out.println("|                                                         |");
+	        System.out.println("+---------------------------------------------------------+");
+
+	}
+
+}
